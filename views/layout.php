dce045373645ea95c80d3a7db3107feb10fa191b
CarloAFlores <carlos2000sistemasc@gmail.com>
2023-07-19 21:49:20 +0600

    Creacion del header y footer (modificar mas)

--- views/layout.php
+++ views/layout.php
@@ -9,13 +9,29 @@ $auth = $_SESSION['login'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Tickets</title>
+    <link rel="stylesheet" href="../../src/css/layout_style.css">
+    <link rel="stylesheet" href="../../node_modules/boxicons/css/boxicons.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500&display=swap" rel="stylesheet">
 
 </head>
 <body>
     <?php if($auth): ?>
         <header>
-            <img src="" alt="">
-            <a href="logout">Cerrar session</a>
+            <div class="header__logo">
+                <img src="../../src/img/logosinfondoblanco.png" alt="" class="header__logo--img">
+            </div>
+
+            <div class="header__menu nav">
+                <div class="nav__menu">
+                    <p>MENU</p>
+                    <button class="nav__menu--btn">
+                        <i class="bx bx-menu bx-sm"></i>
+                    </button>
+                </div>
+                <h5 class="nav__titulo">SISTEMA DE TICKETS</h5>
+            </div>
+
         </header>
     <?php endif; ?>
 
@@ -25,7 +41,7 @@ $auth = $_SESSION['login'];
 <!--Aca es donde aplicamos la funcion anteriormente declarada-->
     <?php if($auth): ?>
         <footer>
-            <h1>FOOTER</h1>
+            <h3>Desarrollado BY ECI© 2023</h3>
         </footer>
     <?php  endif;?>
     
