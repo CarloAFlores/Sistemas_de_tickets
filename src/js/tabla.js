3b5c50b468291b567a001a305b08e1473ea895dc
jenn273312 <jenn27.ham@gmail.com>
2023-08-04 16:39:28 -0600

    Creacion de dos modales

--- src/js/tabla.js
+++ src/js/tabla.js
@@ -17,19 +17,12 @@ for(var x = 0;x< tbrow.length;x++){
 
 
 let cerrar = document.querySelectorAll(".close")[0];
-let abrir = document.querySelectorAll(".cta")[0];
+
 let modal = document.querySelectorAll(".primermodal")[0];
 let modalC = document.querySelectorAll(".primermodal-container")[0];
 
 
-//funcion del modal
-abrir.addEventListener("click", function(e) {
-	e.preventDefault();
-	modalC.style.opacity = "1";
-	modalC.style.visibility = "visible";
-	modal.classList.toggle("modal-close");
-});
-
+//funcion del modal1
 cerrar.addEventListener("click",function(){
 	modal.classList.toggle("modal-close");
 
@@ -38,3 +31,37 @@ cerrar.addEventListener("click",function(){
 		modalC.style.visibility = "hidden";
 	},850)
 })
+
+function hola(id){
+	modalC.style.opacity = "1";
+	modalC.style.visibility = "visible";
+	modal.classList.toggle("modal-close");
+
+	console.log(id)
+}
+
+
+//funcion del modal2
+
+let cerrar2 = document.querySelectorAll(".close2")[0];
+let modal2 = document.querySelectorAll(".segundomodal")[0];
+let modalC2 = document.querySelectorAll(".segundomodal-container")[0];
+
+
+cerrar2.addEventListener("click",function(){
+	modal2.classList.toggle("modal-close2");
+
+	setTimeout(function(){
+		modalC2.style.opacity = "0";
+		modalC2.style.visibility = "hidden";
+	},850)
+})
+
+function hola2(id){
+
+	modalC2.style.opacity = "1";
+	modalC2.style.visibility = "visible";
+	modal2.classList.toggle("modal-close2");
+
+	console.log(id)
+}
--- views/paginas/index.php
+++ views/paginas/index.php
@@ -13,6 +13,7 @@ header('Location: pagina_no_encontrada');
 	<link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="../../node_modules/boxicons/css/boxicons.min.css">
 	<link rel="stylesheet" href="../../src/css/style_tabla.css">
+    <link rel="stylesheet" href="../../src/css/modales_style.css">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
@@ -75,15 +76,15 @@ header('Location: pagina_no_encontrada');
                 <td data-label="Opciones">
                    	<div class="opciones_pequeñas"> 
 
-                    	<button href="#" class="boton-li icono cta" title="Status" type="button" id="miboton">
+                    	<button href="#" class="boton-li icono " title="Status" type="button" id="miboton" onclick="hola(<?php echo $ticket->id; ?>)">
                     	    <i class='bx bx-list-ul' style='color:#ffffff'></i>
                     	</button>
 
-                    	<button class="boton-subir icono" title="Archivo">
+                    	<button class="boton-subir icono" title="Archivo" onclick="hola2(<?php echo $ticket->id; ?>)" id="">
                     	    <i class='bx bx-upload' style='color:#ffffff'></i>
                     	</button>
 
-                    	<button class="boton-resubir icono" title="AdRespuesta">
+                    	<button class="boton-resubir icono" title="AdRespuesta" onclick="hola3(<?php echo $ticket->id; ?>)">
                     	    <i class='bx bx-upload' style='color:#ffffff'></i>
                     	</button>
 
@@ -111,7 +112,7 @@ header('Location: pagina_no_encontrada');
 <div class="primermodal-container">
     <div class="primermodal modal-close">
         <p class="close">X</p>
-        <img src="../../src/img/ilustracion.svg" alt="ilustracion">
+        <img src="../../src/img/ilustracion2.svg" alt="ilustracion">
         <div class="modal-textos">
             <h2>STATUS <span> Ticket </span></h2>
 
@@ -119,22 +120,74 @@ header('Location: pagina_no_encontrada');
             <form class="cajita">
                 <div class="cajita__dos">
                     <h4>Status</h4>
-                    <select>
+                    <select class="cajita__dos--opciones">
                         <option value="1">Proceso</option>
                         <option value="2">Validacion</option>
                     </select>
                 </div>
                 <div class="cajita__tres">
                     <h4>Comentario</h4>
-                    <textarea rows="2"></textarea>
+                    <textarea rows="1"></textarea>
                 </div>
+                <div class="cajita__cuatro">
                 <button type="submit" title="subir" class="botonmodal">Guardar</button>
+                </div>
              </form>
 
         </div>
     </div>
 </div>
 <!--se cierra-->
+<!--segundo modal-->
+<div class="segundomodal-container">
+    <div class="segundomodal modal-close2">
+        <p class="close2">X</p>
+        <img src="../../src/img/ilustracion3.svg" alt="ilustracion">
+        <div class="modal-textos2">
+            <h2>UPLOAD <span> Archivo </span></h2>
+
+
+            <form class="cajita2">
+                <div class="cajita__dos2">
+                    <h4>Subir archivo</h4>
+                    <label for="file">Choose file to upload</label>
+                    <input type="file" name="file" accept="image/png, image/jpeg">
+                </div>
+                
+                <div class="cajita__cuatro2">
+                <button type="submit" title="subir" class="botonmodal">Guardar</button>
+                </div>
+             </form>
+
+        </div>
+    </div>
+</div>
+<!--se cierra-->
+<!--tercer modal
+<div class="tercermodal-container">
+    <div class="tercermodal modal-close3">
+        <p class="close3">X</p>
+        <img src="../../src/img/ilustracion4.svg" alt="ilustracion">
+        <div class="modal-textos3">
+            <h2>UPLOAD <span> Archivo </span></h2>
+
+
+            <form class="cajita3">
+                <div class="cajita__dos3">
+                    <h4>Subir archivo</h4>
+                    <label for="file">Choose file to upload</label>
+                    <input type="file" name="file" accept="image/png, image/jpeg">
+                </div>
+                
+                <div class="cajita__cuatro3">
+                <button type="submit" title="subir" class="botonmodal">Guardar</button>
+                </div>
+             </form>
+
+        </div>
+    </div>
+</div>
+se cierra-->
 
 <div class="paginacion">
     <nav aria-label="...">
